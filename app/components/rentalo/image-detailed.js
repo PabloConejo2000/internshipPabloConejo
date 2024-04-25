4bad4fdbc1fa961e647920af5de02d4b8f3d0bdd
PabloConejo2000 <pablo.conejo2000@hotmail.com>
2024-04-25 10:07:52 +0200

    migracion a tailwind completada

---
+++ app/components/rentalo/image-detailed.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+//EXPORTANDO DECORDAORES
+import { action } from '@ember/object';
+
+
+export default class RentalImageComponent extends Component {
+
+
+  @action
+  toggleSize() {
+    this.args.toggleImage();
+
+  }
+
+
+}
